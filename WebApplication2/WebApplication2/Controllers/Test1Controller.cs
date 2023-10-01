72d5f481af78c8355b85bd28b29d08b2d3b04d4e
Nguyen Tu <Nguyen Tu@DESKTOP-4TPKEJC>
2023-10-01 12:25:41 +0700

    a

---
+++ WebApplication2/WebApplication2/Controllers/Test1Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    [RoutePrefix("api/admin")]
+
+    public class Test1Controller : ApiController
+    {
+        [Route("test")]
+        [HttpGet]
+        // GET: Test
+        public IHttpActionResult Index()
+        {
+            return Ok("aaa");
+        }
+
+    }
+    
+}
